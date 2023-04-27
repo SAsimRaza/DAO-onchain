e80a9705dce311866a30dff7f6c2e29a16d636a4
Jahanzeb Hussain <jahanzebhussain@Jahanzebs-Air.home>
2023-04-27 10:34:52 -0400

    new project

--- test/Token.js
+++ test/Token.js
@@ -50,7 +50,75 @@ describe("Governance Smart Contracts", function () {
       const tx = timeLock.connect(voter1).transferOwnership(voter2.address);
       await expect(tx).to.be.revertedWith("Only Admin authorized");
     });
+
+    it("should update voting power after delegation", async () => {
+      await governanceToken.transfer(voter1.address, ethers.utils.parseEther("100"));
+      await governanceToken.connect(voter1).delegate(voter2.address);
+      const voter2VotingPower = await governanceToken.getVotes(voter2.address);
+      expect(voter2VotingPower).to.equal(ethers.utils.parseEther("100"));
+    });
   
+  });
+
+  describe("Deployment", () => {
+    it("should set the correct name and symbol", async () => {
+      expect(await governanceToken.name()).to.equal("Governance Token");
+      expect(await governanceToken.symbol()).to.equal("GT");
+    });
+
+    it("should set the initial total supply to 1,000,000 tokens", async () => {
+      const totalSupply = await governanceToken.totalSupply();
+      expect(totalSupply).to.equal(ethers.BigNumber.from("1000000000000000000000000"));
+    });
+
+    it("should mint all tokens to the contract deployer", async () => {
+      const ownerBalance = await governanceToken.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(ethers.BigNumber.from("1000000000000000000000000"));
+    });
+  });
+
+  describe("Voting", () => {
+    it("should allow delegation of voting power", async () => {
+      await governanceToken.transfer(voter1.address, ethers.utils.parseEther("100"));
+      await governanceToken.connect(voter1).delegate(owner.address);
+      const ownerVotingPower = await governanceToken.getVotes(owner.address);
+      expect(ownerVotingPower).to.equal(ethers.utils.parseEther("100"));
+    });
+
+    it("should update voting power after delegation", async () => {
+      await governanceToken.transfer(voter1.address, ethers.utils.parseEther("100"));
+      await governanceToken.connect(voter1).delegate(voter2.address);
+      const voter2VotingPower = await governanceToken.getVotes(voter2.address);
+      expect(voter2VotingPower).to.equal(ethers.utils.parseEther("100"));
+    });
+  });
+
+  describe("Transfers", () => {
+    it("should transfer tokens between accounts", async () => {
+      await governanceToken.transfer(voter1.address, ethers.utils.parseEther("100"));
+      const voter1Balance = await governanceToken.balanceOf(voter1.address);
+      expect(voter1Balance).to.equal(ethers.utils.parseEther("100"));
+
+      await governanceToken.connect(voter1).transfer(voter2.address, ethers.utils.parseEther("50"));
+      const voter2Balance = await governanceToken.balanceOf(voter2.address);
+      expect(voter2Balance).to.equal(ethers.utils.parseEther("50"));
+    });
+
+    it("should not transfer more tokens than the sender has", async () => {
+      const initialBalance = await governanceToken.balanceOf(owner.address);
+      await expect(
+        governanceToken.transfer(voter1.address, initialBalance.add(ethers.utils.parseEther("1")))
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+    });
+
+    it("should update voting power after transfer", async () => {
+      await governanceToken.transfer(voter1.address, ethers.utils.parseEther("100"));
+      const voter1Balance = await governanceToken.balanceOf(voter1.address);
+      expect(voter1Balance).to.equal(ethers.utils.parseEther("100"));
+
+      const voter1VotingPower = await governanceToken.getVotes(voter1.address);
+      expect(voter1VotingPower).to.equal(ethers.utils.parseEther("100"));
+    });
   });
 });
           
